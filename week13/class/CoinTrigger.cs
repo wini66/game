90ae1c61f0f9ba7d657e40ec5d29b6dad930b104
wini66 <119557644+wini66@users.noreply.github.com>
2023-12-06 17:44:18 +0900

    Create CoinTrigger.cs

---
+++ week13/class/CoinTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Ball")
+        {
+            GameObject.Find("GameManager").SendMessage("GetCoin");
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
